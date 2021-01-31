be404ba79df108d21144a0afde071d4b3e93f2a1
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-01-31 14:28:03 +0100

    Update CollectedSsidEntity.java

--- app/src/main/java/de/mide/ssidcollector/db/CollectedSsidEntity.java
+++ app/src/main/java/de/mide/ssidcollector/db/CollectedSsidEntity.java
@@ -34,7 +34,7 @@ public class CollectedSsidEntity {
     public long id;
 
     /**
-     * Da Date kein von SQLite unterstützter Datentyp ist werden
+     * Da <code>Date</code> kein von SQLite unterstützter Datentyp ist werden
      * entsprechende Konverter-Methoden benötigt, siehe Klasse
      * {@code MeineTypeConverter}.
      */
